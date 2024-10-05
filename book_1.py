88ab2c487e16a304f3a20c6ba7417872ac303345
progprnv <145828371+progprnv@users.noreply.github.com>
2024-10-05 11:17:59 +0530

    Update book_1.py

--- book_1.py
+++ book_1.py
@@ -1,53 +1,51 @@
+class MyBook:
+    def __init__(self, title, author, publication_date):
+        self.title = title
+        self.author = author
+        self.publication_date = publication_date
+        self.borrowed = False
+
 def read_books():
     library = []
-    n = int(input("Enter number of books:"))
+    n = int(input("enter number of books: "))
     for i in range(n):
-        title = input("Enter book title:")
-        author = input("Enter author name:")
-        publication_date = input("Enter publication date:")
+        title = input("enter the book title: ")
+        author = input("enter the author name: ")
+        publication_date = input("enter publication date: ")
 
-        book = {
-            "title": title,
-            "author": author,
-            "publication_date": publication_date,
-            "borrowed": False
-        }
-        
+        book = MyBook(title, author, publication_date)
         library.append(book)
     return library
 
 def borrow_book(library, title):
     for book in library:
-        if book["title"] == title:
-            if book["borrowed"]:  
-                print("'" + title + "' is already borrowed.")
-            else:  
-                book["borrowed"] = True
-                print("You have borrowed:", title)
+        if book.title == title:
+            if book.borrowed:
+                print("'", title, "' is already borrowed.")
+            else:
+                book.borrowed = True
+                print("u have borrowed:", title)
             return
     print("'", title, "' not found in the library.")
 
 def return_book(library, title):
     for book in library:
-        if book["title"] == title:
-            if book["borrowed"]:  
-                book["borrowed"] = False
-                print("You have returned:", title)
-            else:  
-                print("'" + title + "' was not borrowed.")
+        if book.title == title:
+            if book.borrowed:
+                book.borrowed = False
+                print("u have returned:", title)
+            else:
+                print("'", title, "' was not borrowed.")
             return
-    print("'" + title + "' not found in the library.")
+    print("'", title, "' not found in the library.")
 
-def main():  
+def main():
     library = read_books()
     
-    title_to_borrow = input("Enter the title of the book to borrow: ")
+    title_to_borrow = input("enter the title of the book to borrow: ")
     borrow_book(library, title_to_borrow)
     
-
-    title_to_return = input("Enter the title of the book to return: ")
+    title_to_return = input("enter the title of the book to return: ")
     return_book(library, title_to_return)
-
-
 
 main()
